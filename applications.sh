1ab4df80520069e383789cbf65be25b9d7a16559
Vladimir <86943122+Izebergozz@users.noreply.github.com>
2022-11-13 11:56:43 +0700

    Update applications.sh

--- applications.sh
+++ applications.sh
@@ -2,4 +2,4 @@
 exec xbps-install -Syu \
 -u xbps \
 -Syu \
-base-devel pipewire alsa-pipewire libjack-pipewire gimp alacritty dmenu picom neofetch feh pywal vim libXinerama-devel libXft-devel
+base-devel pipewire alsa-pipewire libjack-pipewire wireplumber gimp alacritty dmenu picom neofetch feh pywal neovim libXinerama-devel libXft-devel
