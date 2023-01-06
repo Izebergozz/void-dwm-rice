2edcbb37e15a0db788170fdd3fb751960eba8556
Vladimir <86943122+Izebergozz@users.noreply.github.com>
2023-01-06 22:12:24 +0700

    Update applications.sh

--- applications.sh
+++ applications.sh
@@ -3,5 +3,5 @@ exec xbps-install -Syu \
 -u xbps \
 -Syu \
 base-devel brightnessctl pipewire alsa-pipewire libjack-pipewire wireplumber gimp alacritty dmenu picom \
-neofetch feh pywal neovim libXinerama-devel libXft-devel xsetroot lm_sensors \
+neofetch feh pywal neovim libXinerama-devel libXft-devel xsetroot lm_sensors nerd-fonts-ttf \
 -Syu
