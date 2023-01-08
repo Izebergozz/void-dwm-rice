b0c8a88c6f9751f487ce0c67582efb13f25d0595
Vladimir <86943122+Izebergozz@users.noreply.github.com>
2023-01-08 05:22:01 +0000

    Add files via upload

--- void_install.sh
+++ void_install.sh
@@ -17,14 +17,12 @@ cfdisk $drive
 echo "Enter the root partition:"
 read partition 
 mkfs.ext4 $partition
-read -p "Did you also create efi partition? [y/n]" answer
-if [[ $answer = y]] then
-	echo "Enter EFI partition: "
-	read efipartition
-	mkfs.vfat -F 32 $efipartition
-	mkdir -p /mnt/boot/efi
-	mount $efipartition /mnt/boot/efi/
-fi
+echo "Enter EFI partition: "
+read efipartition
+mkfs.vfat -F 32 $efipartition
+mkdir -p /mnt/boot/efi
+mount $efipartition /mnt/boot/efi/
+
 mount $partition /mnt
 
 # XBPS #
@@ -46,7 +44,7 @@ printf '\033c'
 ln -sf /usr/share/zoneinfo/Asia/Barnaul /etc/localtime
 hwclock --systohc
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-locale-gen
+#locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "KEYMAP=us" > /etc/vconsole.conf
 echo "Hostname: "
