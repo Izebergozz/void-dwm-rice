2fbe90ac2bb575a780a074e8da48e572af49b2b0
Vladimir <86943122+Izebergozz@users.noreply.github.com>
2023-01-16 19:42:58 +0700

    Update void_install.sh

--- void_install.sh
+++ void_install.sh
@@ -79,7 +79,7 @@ xbps-install grub-x86_64-efi
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id="Void"
 
 # POST-INSTALATION #
-xbps-install -Syu base-devel brightnessctl pipewire alsa-pipewire libjack-pipewire wireplumber gimp alacritty dmenu picom \
+xbps-install -Syu base-devel brightnessctl pipewire alsa-pipewire libjack-pipewire wireplumber pulsemixer gimp alacritty dmenu picom \
 neofetch feh pywal neovim libXinerama-devel libXft-devel xsetroot lm_sensors nerd-fonts-ttf git \
 
 # FINALIZATION #
